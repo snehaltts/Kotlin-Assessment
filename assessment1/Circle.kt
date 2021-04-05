9f85416ad284714df39b22971ce45a3556f4fe37
snehaltts <78852647+snehaltts@users.noreply.github.com>
2021-04-05 09:20:48 +0530

    Update Circle.kt

--- assessment1/Circle.kt
+++ assessment1/Circle.kt
@@ -10,12 +10,19 @@ fun AreaOfCircle(radius:Double): Double{
 fun PerimeterOfCircle(radius:Double): Double{
     return 2*PI*radius
 }
+fun LenOfSideOfSquare(radius:Double): Double{
+    val x = (PI * radius)/ 2
+    return x
+}
 fun main(){
     val sc=Scanner(System.`in`)
     println("Enter the length of the radious: ")
     val radius=sc.nextDouble()
     val areaOfCircle = AreaOfCircle(radius)
     val perimeterOfCircle = PerimeterOfCircle(radius)
+    val LenOfSideOfSquare = LenOfSideOfSquare(radius)
     println("Area of circle:$areaOfCircle")
     println("Perimeter of circle:$perimeterOfCircle")
+    println("Length of circle:$LenOfSideOfSquare")
+
 }
